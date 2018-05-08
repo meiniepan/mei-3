b04def9d6048dd931c4e9173222f5b436df65d6b
ngreedy <hjn910@163.com>
2018-05-08 11:38:06 +0800

    logout

--- app/src/main/java/com/wuyou/worker/view/activity/SettingActivity.java
+++ app/src/main/java/com/wuyou/worker/view/activity/SettingActivity.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import com.gs.buluo.common.network.BaseResponse;
-import com.gs.buluo.common.network.BaseSubscriber;
 import com.gs.buluo.common.network.QueryMapBuilder;
 import com.gs.buluo.common.utils.AppManager;
 import com.gs.buluo.common.utils.DataCleanManager;
@@ -103,38 +101,13 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
                 .doLogout(CarefreeDaoSession.getInstance().getUserId(), QueryMapBuilder.getIns().buildPost())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new BaseSubscriber<BaseResponse>() {
-                    @Override
-                    public void onSuccess(BaseResponse baseResponse) {
-                    }
+                .subscribe(baseResponse -> {
                 });
         CarefreeDaoSession.getInstance().clearUserInfo();
         Intent intent = new Intent(getCtx(), LoginActivity.class);
         startActivity(intent);
         finish();
         AppManager.getAppManager().finishActivity(MainActivity.class);
-//        CarefreeRetrofit.getInstance().createApi(UserApis.class).
-//                doLogout(CarefreeDaoSession.getInstance().getUserId(), com.wuyou.user.network.QueryMapBuilder.getIns().buildPost())
-//                .subscribeOn(Schedulers.io())
-//                .flatMap(new Function<BaseResponse, ObservableSource<?>>() {
-//                    @Override
-//                    public ObservableSource<?> apply(BaseResponse baseResponse) throws Exception {
-//                        CarefreeApplication.getInstance().getUserInfoDao().deleteAll();
-//                        CarefreeApplication.getInstance().setUserInfo(null);
-//                        return (ObservableSource) observer -> {};
-//                    }
-//                })
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new com.wuyou.user.network.BaseSubscriber() {
-//                    @Override
-//                    public void onSuccess(Object o) {
-//                        Intent intent = new Intent();
-//                        intent.setClass(getCtx(), LoginActivity.class);
-//                        startActivity(intent);
-////        AppManager.getAppManager().finishActivity(MainActivity.class);
-//                        finish();
-//                    }
-//                });
     }
 
     @Override
