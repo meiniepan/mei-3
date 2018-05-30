13de00adac7c5e60856e8014d85413ade35a53ed
wangboning <meiniepan@163.com>
2018-05-30 15:47:34 +0800

    单设备登录

--- app/src/main/java/com/wuyou/worker/network/CarefreeHttpInterceptor.java
+++ app/src/main/java/com/wuyou/worker/network/CarefreeHttpInterceptor.java
@@ -3,10 +3,13 @@ package com.wuyou.worker.network;
 import android.text.TextUtils;
 import android.util.Base64;
 
+import com.google.gson.Gson;
 import com.gs.buluo.common.BaseApplication;
 import com.gs.buluo.common.network.EncryptUtil;
 import com.gs.buluo.common.utils.Utils;
 import com.wuyou.worker.CarefreeDaoSession;
+import com.wuyou.worker.bean.entity.AuthTokenEntity;
+import com.wuyou.worker.util.EncodeUtil;
 
 import java.io.IOException;
 
@@ -19,6 +22,8 @@ import okhttp3.Response;
  * Created by hjn on 2016/11/10.
  */
 public class CarefreeHttpInterceptor implements Interceptor {
+    private String sign;
+
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request req = chain.request();
@@ -27,14 +32,31 @@ public class CarefreeHttpInterceptor implements Interceptor {
         String query = url.encodedQuery();
         if (!TextUtils.isEmpty(query) && !query.contains("sign=")) {
             HttpUrl.Builder newBuilder = url.newBuilder();
-            newBuilder.addQueryParameter("sign", EncryptUtil.getSha1(Base64.encode(query.getBytes(), Base64.NO_WRAP)).toUpperCase());
+            sign = EncryptUtil.getSha1(Base64.encode(query.getBytes(), Base64.NO_WRAP)).toUpperCase();
+            newBuilder.addQueryParameter("sign", sign);
             url = newBuilder.build();
         }
         if (CarefreeDaoSession.getInstance().getUserInfo() != null) {
             builder.addHeader("Authorization", CarefreeDaoSession.getInstance().getUserInfo().getToken());
         }
+        try {
+            AddAuthToken(builder);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Request request = builder.addHeader("Accept", "application/json").url(url).addHeader("Content-Type", "application/json").
                 addHeader("User-Agent", Utils.getDeviceInfo(BaseApplication.getInstance().getApplicationContext())).build();
         return chain.proceed(request);
+    }
+    private void AddAuthToken(Request.Builder builder) throws Exception {
+        AuthTokenEntity e = new AuthTokenEntity();
+        if (CarefreeDaoSession.getInstance().getUserInfo() != null)
+            e.client_id = CarefreeDaoSession.getInstance().getUserInfo().getWorker_id();
+        e.client = "worker";
+        e.lng = 116.36968727736242d;
+        e.lat = 39.89528271571901d;
+        Gson gson = new Gson();
+        String sInfomation = gson.toJson(e);
+        builder.addHeader("AuthToken", EncodeUtil.get3DES(sInfomation, sign));
     }
 }
