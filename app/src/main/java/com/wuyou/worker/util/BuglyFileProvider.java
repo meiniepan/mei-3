92235b010f998afe2ad66e533d7cd09508ac480e
ngreedy <hjn910@163.com>
2018-05-30 15:52:18 +0800

    分包啦   图片选择库

---
+++ app/src/main/java/com/wuyou/worker/util/BuglyFileProvider.java
@@ -0,0 +1,10 @@
+package com.wuyou.worker.util;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by DELL on 2018/5/30.
+ */
+
+public class BuglyFileProvider extends FileProvider {
+}
